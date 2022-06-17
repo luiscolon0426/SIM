e618f48d9ea51a98c07cb3f200960fb9fb4e5f85
gabriel337 <g.davind.lopezruiz@gmail.com>
2022-06-17 15:19:53 -0400

    add beta proyect

--- signin/signin.py
+++ signin/signin.py
@@ -1,67 +1,59 @@
-'''
-Sign in screen
-'''
-import hashlib
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
 from kivy.lang import Builder
-from pymongo import MongoClient
-Builder.load_file('signin/signin.kv')
 
+from pymongo import MongoClient
+import hashlib
+
+Builder.load_file('signin/signin.kv')
 
 class SigninWindow(BoxLayout):
     def __init__(self, **kwargs):
-        '''
-        Constructor
-        '''
         super().__init__(**kwargs)
 
     def validate_user(self):
-        '''
-        comment
-        '''
         client = MongoClient()
-        db = client.sim
+        db = client.silverpos
         users = db.users
-        user = self.ids.usr_field
+
+        user = self.ids.username_field
         pwd = self.ids.pwd_field
         info = self.ids.info
+
         uname = user.text
         passw = pwd.text
+
         user.text = ''
-        passw.text = ''
+        pwd.text = ''
+
         if uname == '' or passw == '':
-            info.text = '[color=#FF0000]username and/or password required[/color]'
+            info.text = '[color=#FF0000]username and/ or password required[/color]'
         else:
             user = users.find_one({'user_name':uname})
+
             if user == None:
-                info.text = '[color=#FF0000]Invalid username and/or password[/color]'
+                info.text = '[color=#FF0000]Invalid Username and/or Password[/color]'
             else:
                 passw = hashlib.sha256(passw.encode()).hexdigest()
                 if passw == user['password']:
                     des = user['designation']
-                    #info.text = '[color=#00FF00]Logged In successfully!!![/color]'
+                    # info.text = '[color=#00FF00]Logged In successfully!!![/color]'
                     info.text = ''
-                    self.parent.parent.parent.ids.scrn_op.children[0].ids.loggedin_user.text = uname
+                    self.parent.parent.parent\
+                        .ids.scrn_op.children[0]\
+                            .ids.loggedin_user.text = uname
                     if des == 'Administrator':
                         self.parent.parent.current = 'scrn_admin'
                     else:
                         self.parent.parent.current = 'scrn_op'
                 else:
-                    info.text = '[color=#FF0000]Invalid username and/or password[/color]'
+                    info.text = '[color=#FF0000]Invalid Username and/or Password[/color]'
 
 
 class SigninApp(App):
     def build(self):
-        '''
-        comment
-        '''
         return SigninWindow()
 
-
-if __name__ == "__main__":
-    '''
-    run the app
-    '''
-    SigninApp().run()
+if __name__=="__main__":
+    sa = SigninApp()
+    sa.run()
--- utils/datatable.py
+++ utils/datatable.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
+
 from pymongo import MongoClient
 from collections import OrderedDict
 
@@ -18,7 +19,6 @@ Builder.load_string('''
             size_hint_y: None
             height: self.minimum_height
             spacing: 5
-
 <CustLabel@Label>:
     bcolor: (1,1,1,1)
     canvas.before:
@@ -27,124 +27,41 @@ Builder.load_string('''
         Rectangle:
             size: self.size
             pos: self.pos
-
-
 ''')
 class DataTable(BoxLayout):
-    def __init__(self, **kwargs):
+    def __init__(self,table='', **kwargs):
         super().__init__(**kwargs)
-        
-        products = self.get_products()
+
+        # products = self.get_products()
+        products = table
+
+         #     stb = {
+    #     'TH0':{0:'St0',1:'Sample1',2:'Sample2',3:'Sample4'},
+    #     'TH1':{0:'Stm0',1:'Sample1',2:'Sample2',3:'Sample4'},
+    #     'TH2':{0:'Stmp0',1:'Sampled1.0',2:'Sampled2.0',3:'Sampled4.0'},
+    #     'TH3':{0:'Stmpl0',1:'Sample1',2:'Sample2',3:'Sample4'},
+    #     'TH4':{0:'Stmple0',1:'Sample1',2:'Sample2',3:'Sample4'},
 
         col_titles = [k for k in products.keys()]
         rows_len = len(products[col_titles[0]])
         self.columns = len(col_titles)
-        #print(rows_len)
+        # print(rows_len)
         table_data = []
         for t in col_titles:
-            table_data.append({'text':str(t), 'size_hint_y':None, 'height': 50, 'bcolor':(.06,45,45,1)})
-
+            table_data.append({'text':str(t),'size_hint_y':None,'height':50,'bcolor':(.06,.45,.45,1)})
 
         for r in range(rows_len):
             for t in col_titles:
-                table_data.append({'text':str(products[t][r]),'size_hint_y':None,'height':30, 'bcolor':(.06,.25,.25,1)})
+                table_data.append({'text':str(products[t][r]),'size_hint_y':None,'height':30,'bcolor':(.06,.25,.25,1)})
         self.ids.table_floor_layout.cols = self.columns
         self.ids.table_floor.data = table_data
+
+
     
-    def get_products(self):
-        client = MongoClient()
-        db = client.silverpos
-        products = db.stocks
-        _stocks = OrderedDict()
-        _stocks['product_code'] = {}
-        _stocks['product_name'] = {}
-        _stocks['product_weight'] = {}
-        _stocks['in_stock'] = {}
-        _stocks['sold'] = {}
-        _stocks['order'] = {}
-        _stocks['last_purchase'] = {}
+# class DataTableApp(App):
+#     def build(self):
 
-        product_code = []
-        product_name = []
-        product_weight = []
-        in_stock = []
-        sold = []
-        order = []
-        last_purchase = []
+#         return DataTable()
 
-        for product in products.find():
-            product_code.append(product['product_code'])
-            product_name.append(product['product_name'])
-            product_weight.append(product['product_weight'])
-            in_stock.append(product['in_stock'])
-            sold.append(product['sold'])
-            order.append(product['order'])
-            last_purchase.append(product['last_purchase'])
-            # print(designations)
-        products_length = len(product_code)
-        idx = 0
-        while idx < products_length:
-            _stocks['product_code'][idx] = product_code[idx]
-            _stocks['product_name'][idx] = product_name[idx]
-            _stocks['product_weight'][idx] = product_weight[idx]
-            _stocks['in_stock'][idx] = in_stock[idx]
-            _stocks['sold'][idx] = sold[idx]
-            _stocks['order'][idx] = order[idx]
-            _stocks['last_purchase'][idx] = last_purchase[idx]
-
-            idx += 1
-        return _stocks
-
-
-    def get_products(self):
-        client = MongoClient()
-        db = client.silverpos
-        products = db.stocks
-        _stocks = OrderedDict()
-        _stocks['product_code'] = {}
-        _stocks['product_name'] = {}
-        _stocks['product_weight'] = {}
-        _stocks['in_stock'] = {}
-        _stocks['sold'] = {}
-        _stocks['order'] = {}
-        _stocks['last_purchase'] = {}
-
-        product_code = []
-        product_name = []
-        product_weight = []
-        in_stock = []
-        sold = []
-        order = []
-        last_purchase = []
-
-        for product in products.find():
-            product_code.append(product['product_code'])
-            product_name.append(product['product_name'])
-            product_weight.append(product['product_weight'])
-            in_stock.append(product['in_stock'])
-            sold.append(product['sold'])
-            order.append(product['order'])
-            last_purchase.append(product['last_purchase'])
-            # print(designations)
-        products_length = len(product_code)
-        idx = 0
-        while idx < products_length:
-            _stocks['product_code'][idx] = product_code[idx]
-            _stocks['product_name'][idx] = product_name[idx]
-            _stocks['product_weight'][idx] = product_weight[idx]
-            _stocks['in_stock'][idx] = in_stock[idx]
-            _stocks['sold'][idx] = sold[idx]
-            _stocks['order'][idx] = order[idx]
-            _stocks['last_purchase'][idx] = last_purchase[idx]
-
-            idx += 1
-        return _stocks
-
-
-
-class DataTableApp(App):
-    def build(self):
-        return DataTable()
-
-if __name__=='__main__':
-    DataTableApp().run()
+# if __name__=='__main__':
+#     DataTableApp().run()
